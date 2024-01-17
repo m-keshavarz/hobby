bb9ceef914cbe04b1f1c76400142bbfe9002b897
m-keshavarz <m.k.dev@outlook.com>
2024-01-17 23:37:53 +0330

    add picture and it's styles

--- src/app/portfolio1/index.tsx
+++ src/app/portfolio1/index.tsx
@@ -1,22 +1,38 @@
-import { Box, Button, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
 
 const Portfolio1 = () => {
   return (
     <Box>
-      <Box>
-        <Box mt="12.5rem" fontSize="2.75rem" fontWeight="700">
-          <Text>Hi, I am John,</Text>
-          <Text>Creative Technologist</Text>
+      <Flex justifyContent="space-between" mt="12.5rem">
+        <Box w="47%">
+          <Box fontSize="2.75rem" fontWeight="700">
+            <Text>Hi, I am John,</Text>
+            <Text>Creative Technologist</Text>
+          </Box>
+          <Text mt="2.5rem">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </Text>
+          <Button
+            mt="2.38rem"
+            background="#FF6464"
+            color="#FFF"
+            w="13rem"
+            _hover={{ background: "#DD4242" }}
+          >
+            Download Resume
+          </Button>
         </Box>
-        <Text mt="2.5rem" w="45%">
-          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-          sint. Velit officia consequat duis enim velit mollit. Exercitation
-          veniam consequat sunt nostrud amet.
-        </Text>
-        <Button  mt="2.38rem">
-          Download Resume
-        </Button>
-      </Box>
+        <Box
+          boxSize="fit-content"
+          rounded="full"
+          mr="9.4rem"
+          boxShadow="-8px 17px 5px #EDF7FA"
+        >
+          <Image src="/public/portrait.png" alt="portraint of John" />
+        </Box>
+      </Flex>
     </Box>
   );
 };
